67b211efca29add19a9312e1067dc43068af38aa
William Bechay <72413042+WilliamBechay@users.noreply.github.com>
2025-12-25 12:48:22 -0500

    Update Supabase configuration with new publishable key

--- src/lib/customSupabaseClient.js
+++ src/lib/customSupabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://gxzicdjsyxsaxhsqdfko.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd4emljZGpzeXhzYXhoc3FkZmtvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUwODYwMjIsImV4cCI6MjA4MDY2MjAyMn0.4TZMhgzTbF_lZ8aq8e745xM_iQTkwXF5kQbUA_XH4nQ';
+const supabaseAnonKey = 'sb_publishable_YH3VurNFDsKExRaIb_4Ijg_yjL2RCxG';
 
 const customSupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
 
